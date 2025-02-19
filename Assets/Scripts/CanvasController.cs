8050582cf2fe92540b6aa92c67ddccd7c2632fc8
Ramazan  TOY <ramcotoy06@gmail.com>
2025-02-19 18:03:51 +0300

    Unused CanvasController codes removed,SkillUI added to scene

--- Assets/Scripts/CanvasController.cs
+++ Assets/Scripts/CanvasController.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class CanvasController : MonoBehaviour
 {
-    [SerializeField] private VariableJoystick _joystick;
-    
-    
-    public VariableJoystick Joystick => _joystick;
+
 
 }
